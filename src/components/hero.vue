a5bc85e6e6620e3132cc53feb1c5c41c9d19797c
FraAccaBi <francescahuda@gmail.com>
2022-05-02 16:46:57 +0200

    bonus

---
+++ src/components/hero.vue
@@ -0,0 +1,8 @@
+<template>
+  <mdb-jumbotron class="mb-0 text-center">
+    <mdb-card-title class="pb-2 h4"><strong>My adventure</strong></mdb-card-title>
+    <mdb-view class="my-4" src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).webp" alt="">
+      <a><mdb-mask waves overlay="white-slight"/></a>
+    </mdb-view>
+  </mdb-jumbotron>
+</template>
